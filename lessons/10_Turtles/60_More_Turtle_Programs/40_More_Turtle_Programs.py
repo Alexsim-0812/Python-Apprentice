aa151db521d7df1c44eae6f91068fc68053d8cc3
Alex <sdshim@gmail.com>
2026-03-17 00:01:59 +0000

    Week 2:worked on using loop,and variables.

--- lessons/10_Turtles/60_More_Turtle_Programs/40_More_Turtle_Programs.py
+++ lessons/10_Turtles/60_More_Turtle_Programs/40_More_Turtle_Programs.py
@@ -6,8 +6,28 @@ Then change the code so that the turtle has a different image ( look in the 'ima
 directory ) and when you click on it, it moves to a random location on the screen.
 
 Use this code to get a random x and y location
-
-    import random
-    x = random.randint(-300, 300)
-    y = random.randint(-300, 300)
 """
+import random
+x = random.randint(-300, 300)
+y = random.randint(-300, 300)
+
+import turtle as turtle
+
+turtle.setup(width=600, height=600)
+
+t = turtle.Turtle()
+
+t.shape("turtle")
+t.turtlesize(stretch_wid=10, stretch_len=10, outline=4) # Make the turtle really big
+
+def turtle_clicked(t, x, y):
+   
+    print('turtle clicked!')
+    
+    for i in range(0,360, 20): # Full circle, 20 degrees at a time
+        t.tilt(20) # Tilt the turtle 20 degrees
+
+# Connect the turtle to the turtle_clicked function
+t.onclick(lambda x, y, t=t: turtle_clicked(t, x, y))
+
+turtle.done() # Important! Use `done` not `exitonclick` to keep the window open
