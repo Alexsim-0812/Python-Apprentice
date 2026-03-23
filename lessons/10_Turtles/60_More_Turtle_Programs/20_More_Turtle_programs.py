e8cae979eb4ed718dfe294707abe2737befed77f
Alex <sdshim@gmail.com>
2026-03-23 23:58:34 +0000

    week3:happy coding:)

--- lessons/10_Turtles/60_More_Turtle_Programs/20_More_Turtle_programs.py
+++ lessons/10_Turtles/60_More_Turtle_Programs/20_More_Turtle_programs.py
@@ -5,3 +5,4 @@ from the section "Change the Turtle Image"
 Then change the code so that the turtle has a different image ( look in the 'images'
 directory ) and moves to the corners of the screen in a square pattern. 
 """
+
